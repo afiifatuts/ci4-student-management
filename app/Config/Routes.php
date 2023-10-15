d010f329be9c1e95d759aded134feaedfaf48d6a
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-10-15 21:16:12 +0700

    melindungi routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,15 +5,17 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Layout::index');
+$routes->get('/layout', 'Layout::index',['filter'=>'ceklogin']);
+$routes->get('/layout/(:any)', 'Layout::$1',['filter'=>'ceklogin']);
 
 
 $routes->get('/login', 'Login::index');
 $routes->post('/login/cekuser', 'Login::cekuser');
 $routes->get('/login/keluar', 'Login::keluar');
-
-$routes->get('/layout', 'Layout::index');
-$routes->get('/mahasiswa', 'Mahasiswa::index');
+//melindungi route
+$routes->get('/mahasiswa', 'Mahasiswa::index',['filter'=>'ceklogin']);
+$routes->get('/mahasiswa/(:any)', 'Mahasiswa::$1',['filter'=>'ceklogin']);
 $routes->get('/mahasiswa/ambildata', 'Mahasiswa::ambildata');
 $routes->get('/mahasiswa/formtambah', 'Mahasiswa::formtambah');
 $routes->post('/mahasiswa/simpandata', 'Mahasiswa::simpandata');
